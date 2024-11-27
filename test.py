9ed6441d971c5f9a457ec358e2f788b015919254
Chen Xu <cxu.self@gmail.com>
2024-11-27 19:44:45 +0200

    Add point cloud conversion utility and update test script

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# Copyright (c) 2018-2023 www.open3d.org
-# SPDX-License-Identifier: MIT
-# ----------------------------------------------------------------------------
-
 import open3d as o3d
 
-if __name__ == "__main__":
-    pcd_data = o3d.data.PCDPointCloud()
-    print(
-        f"Reading pointcloud from file: fragment.pcd stored at {pcd_data.path}")
-    pcd = o3d.io.read_point_cloud(pcd_data.path)
-    print(pcd)
-    print("Saving pointcloud to file: copy_of_fragment.pcd")
-    o3d.io.write_point_cloud("copy_of_fragment.pcd", pcd)
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+o3d.visualization.draw_geometries([pcd])
